4b8ce18192bc4fc75d8db967c45a881329cd5c28
Victor KVV <kvv_post@mail.ru>
2024-01-14 18:05:31 +0300

    Step 2 attempt 2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,5 @@
 package hexlet.code;
-import hexlet.code.Validator;
-import hexlet.code.StringSchema;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
