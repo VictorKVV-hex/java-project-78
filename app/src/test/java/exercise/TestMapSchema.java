31f7ca33dbae66f0bc010cfdfb32916c4452710d
KVV <kvvpost@gmail.com>
2024-01-18 17:49:07 +0300

    Middle of Step4 (Map)

---
+++ app/src/test/java/exercise/TestMapSchema.java
@@ -0,0 +1,40 @@
+package exercise;
+
+import hexlet.code.MapSchema;
+import hexlet.code.NumberSchema;
+import hexlet.code.Validator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMapSchema {
+    private MapSchema schema;
+
+    @BeforeEach
+    public void beforeEach() {
+        Validator v = new Validator();
+        schema = v.map();
+    }
+    @Test
+    public void requiredTest() {
+        assertTrue(schema.isValid(null));
+
+        schema.required();
+
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(new HashMap()));
+        Map<String, String> data = new HashMap<>();
+        data.put("key1", "value1");
+        assertTrue(schema.isValid(data));
+
+        schema.sizeof(2);
+        assertFalse(schema.isValid(data));
+        data.put("key2", "value2");
+        assertTrue(schema.isValid(data));
+    }
+}
